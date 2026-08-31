23fafea0518a93eabfe3a9d3ae3a3221a0017eca
Kabi <kabim01@yahoo.com>
2026-08-31 13:30:50 +0200

    Fix header size and PWA cache

--- sw.js
+++ sw.js
@@ -1,12 +1,17 @@
-const CACHE_NAME = "studentbevis-v1";
+const CACHE_NAME = "studentbevis-v2";
 
 const filesToCache = [
     "./",
     "./index.html",
     "./styles.css",
-    "./script.js"
+    "./script.js",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
+
+// Install new version
 self.addEventListener("install", event => {
 
     event.waitUntil(
@@ -18,18 +23,61 @@ self.addEventListener("install", event => {
 
     );
 
+    self.skipWaiting();
 });
 
 
+// Delete old cache versions
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(cacheNames => {
+
+            return Promise.all(
+
+                cacheNames.map(cache => {
+
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+    self.clients.claim();
+});
+
+
+// Try internet first,
+// use cache if internet is unavailable
 self.addEventListener("fetch", event => {
 
     event.respondWith(
 
-        caches.match(event.request)
+        fetch(event.request)
+
             .then(response => {
 
-                return response ||
-                    fetch(event.request);
+                const copy = response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+                        cache.put(event.request, copy);
+                    });
+
+                return response;
+
+            })
+
+            .catch(() => {
+
+                return caches.match(event.request);
 
             })
 
